1d64397419a1ce0220e02878826a2324c9432610
EnshenZhu <winniecmz.zhu@gmail.com>
2024-03-14 12:48:15 -0400

    minor improve 2

--- DL_basic/ActivationFunction/softmax.py
+++ DL_basic/ActivationFunction/softmax.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def softmax(x_ls, beta=1):
-    exp_x = np.exp(beta * x_ls)
+def softmax(x, beta=1):
+    exp_x = np.exp(beta * x)
     return exp_x / exp_x.sum(axis=0, keepdims=True)
 
 
-x = np.random.rand(500) * 10 - 5
-y = softmax(x, beta=5)
+X = np.random.rand(500) * 10 - 5
+y = softmax(X, beta=5)
 
-plt.scatter(x, y)
+plt.scatter(X, y)
 plt.show()
