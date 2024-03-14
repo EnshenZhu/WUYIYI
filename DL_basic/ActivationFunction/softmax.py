4067a5653ba465f59177a0c21148a0f75ac4f35b
EnshenZhu <winniecmz.zhu@gmail.com>
2024-03-14 12:41:56 -0400

    add beta to the function

--- DL_basic/ActivationFunction/softmax.py
+++ DL_basic/ActivationFunction/softmax.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def softmax(x_ls):
-    exp_x = np.exp(x_ls)
+def softmax(x_ls, beta=1):
+    exp_x = np.exp(beta * x_ls)
     return exp_x / exp_x.sum(axis=0, keepdims=True)
 
 
-x = np.random.rand(500) * 50 - 25
-y = softmax(x)
+x = np.random.rand(500) * 10 - 5
+y = softmax(x, beta=5)
 
 plt.scatter(x, y)
 plt.show()
